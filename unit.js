d2884ebbcb51216faf58855123b54dbae3e4c77a
Ethan Arns <narkro555@gmail.com>
2018-09-15 12:43:13 -0700

    Set up Node-based unit testing

--- unit.js
+++ unit.js
@@ -1,11 +1,13 @@
 const BABYLON = require('babylonjs');
-var assert = require('chai').assert;
-
+const assert = require('chai').assert;
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const Globals = require('./components/Globals.js');
 const Player = require('./components/Player.js');
 const Brick = require('./components/Brick.js');
 const Utils = require('./components/Utils.js');
-var html = require('create-html')({
-    title: 'example'
+var html_gen = require('create-html')({
+    title: 'Block.JS unit testing'
 });
-console.log(html);
+const dom = new JSDOM(html_gen);
+console.log(dom);
