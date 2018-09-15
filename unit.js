093ca7854672c30318f627cc4da86beea0c17a35
Ethan Arns <narkro555@gmail.com>
2018-09-15 11:35:27 -0700

    Adding unit testing

---
+++ unit.js
@@ -0,0 +1,6 @@
+const BABYLON = require('babylonjs');
+const chai = require('chai');
+require('./components/Globals.js');
+require('./components/Brick.js');
+require('./components/Player.js');
+require('./components/Utils.js');
