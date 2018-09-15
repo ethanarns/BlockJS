7041568c97dda2b221401ec4c3867d005869846b
Ethan Arns <narkro555@gmail.com>
2018-09-14 22:10:57 -0700

    Removed old UI class

--- components/UI.js
+++
@@ -1,8 +0,0 @@
-// Create crosshair
-var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI");
-var guiCircle = new BABYLON.GUI.Ellipse();
-    guiCircle.height = "10px";
-    guiCircle.width = "10px";
-    guiCircle.color = "White";
-    guiCircle.thickness = 2;
-advancedTexture.addControl(guiCircle);
