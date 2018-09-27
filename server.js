330d6a2799d4fed09e7cb2f73c395bf3889b4b16
Ethan Arns <narkro555@gmail.com>
2018-09-27 15:48:14 -0700

    Adding postgres integration

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const { Pool } = require('pg');
+const pool = new Pool();
 
-app.listen(PORT, function() {
-    console.log("Listening on port " + PORT);
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+const PORT = 3001;
+
+app.use("/libs", express.static(path.join(__dirname, 'libs')));
+app.use("/components", express.static(path.join(__dirname, "components")));
+app.use("/audio", express.static(path.join(__dirname, "audio")));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post("/save", function(req, res) {
+
+});
+
+var server = app.listen(PORT, function() {
+    console.log("Listening on port " + server.address().port);
 });
